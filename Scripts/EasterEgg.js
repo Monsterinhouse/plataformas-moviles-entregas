e1638e5b818d55c287ff2484f4f5ea6b51f73e68
Monsterinhouse <lucas.cruz2003@hotmail.com>
2022-03-30 22:49:20 -0300

    para fabri

--- Scripts/EasterEgg.js
+++ Scripts/EasterEgg.js
@@ -1,8 +1,8 @@
 const easterEgg =
-  "monsterinhouse";
+  "kaz";
 
 const validKeys = [
-  "m","o","n","s","t","e","r","i","h","u"
+  "k","a","z"
 ];
 
 let currInput = "";
@@ -11,13 +11,13 @@ const checkEasterEgg = (ev) => {
   if (!validKeys.includes(ev.key)) return;
   currInput += (ev.key);
 
-  if (currInput === easterEgg) {
-    alert("TODO HA CAMBIADO");
+  if (currInput.includes(easterEgg)) {
+    alert("Andas de curioso/a?");
     container.innerHTML +=
-      "<iframe src='https://www.youtube.com/embed/cikFpGwIS6U?autoplay=1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+      "<div id='eeg'><iframe src='https://www.youtube.com/embed/cikFpGwIS6U?autoplay=1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><div>";
   }
 
-  if (currInput.length >= easterEgg.length) {
+  if (currInput.length >= easterEgg.length * 5) {
     currInput = "";
   }
 };
